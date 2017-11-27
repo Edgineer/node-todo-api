a32b24f8ddfde17401140ff0cb0abc99010f5b0f
Edgineer <elopez42@g.ucla.edu>
2017-11-27 10:46:16 -0800

    add POST /users/login route Login authentication

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,25 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (username, password) {
+  var User = this;
+
+  return User.findOne({username}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve,reject) => {
+      bcrypt.compare(password,user.password,(err,res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
   if (user.isModified('password')) {
